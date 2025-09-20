468035560fd8c36ceb651da5bfa2b027fef7d679
Polishskill21 <rafalmiszkiewicz6@gmail.com>
2025-09-20 19:55:29 +0200

    feat: added a new local

---
+++ lib/localization/app_localizations_delegate.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:attendly/localization/app_de.dart';
+import 'package:attendly/localization/app_en.dart';
+import 'package:attendly/localization/app_localizations.dart';
+
+class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+  const AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en', 'de'].contains(locale.languageCode);
+
+  @override
+  Future<AppLocalizations> load(Locale locale) {
+    return _load(locale);
+  }
+
+  static Future<AppLocalizations> _load(Locale locale) async {
+    switch (locale.languageCode) {
+      case 'en':
+        return AppLocalizationsEn();
+      case 'de':
+        return AppLocalizationsDe();
+      default:
+        return AppLocalizationsEn();
+    }
+  }
+
+  static AppLocalizations getLocalization(Locale locale) {
+    switch (locale.languageCode) {
+      case 'en':
+        return AppLocalizationsEn();
+      case 'de':
+        return AppLocalizationsDe();
+      default:
+        return AppLocalizationsEn();
+    }
+  }
+
+  @override
+  bool shouldReload(LocalizationsDelegate<AppLocalizations> old) => false;
+}
