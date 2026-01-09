8b7cdad79087ac185cf98f71820fe513d908a0ea
Polishskill21 <rafalmiszkiewicz6@gmail.com>
2026-01-10 00:14:56 +0100

    feat: app version 1.2.2

--- lib/frontend/pages/settings_page/settings_page.dart
+++ lib/frontend/pages/settings_page/settings_page.dart
@@ -264,7 +264,7 @@ class _SettingsPageState extends State<SettingsPage> {
                     children: [
                       Expanded(
                         child: Text(
-                          localizations.getAppsVerision("1.2.1"),
+                          localizations.getAppsVerision("1.2.2"),
                           textAlign: TextAlign.center,
                           style: TextStyle(
                             fontSize: ResponsiveUtils.getBodyFontSize(context),
