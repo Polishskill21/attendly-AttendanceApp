238dbb965f8c21d26d8eb2cbe17f7ebbcfd43413
Polishskill21 <rafalmiszkiewicz6@gmail.com>
2026-01-24 17:53:47 +0100

    feat: the database core

---
+++ lib/data/local/database.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:attendly/data/local/tables/dialy_entry_table.dart';
+import 'package:attendly/data/local/tables/directory_people_table.dart';
+import 'package:attendly/data/local/tables/weekly_entry_table.dart';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+
+part 'database.g.dart';
+
+@DriftDatabase(tables: [DirectoryPeople, DailyEntry, WeeklyEntry])
+class AppDatabase extends _$AppDatabase{
+  AppDatabase(super.executor);
+
+  AppDatabase.testInstance() : super(
+    NativeDatabase.memory(setup: (db) {
+      db.execute('PRAGMA foreign_keys = ON');
+    }),
+  );
+
+  @override
+  int get schemaVersion => 1;
+
+  Future<void> forceOpen() async {
+    await customSelect('SELECT 1').getSingle();
+  }
+
+  /// First call the 
+  static QueryExecutor createExecutor(File filename){
+    return NativeDatabase(
+      filename,
+      setup: (db) {
+        db.execute('PRAGMA foreign_keys = ON;');
+      },
+    );
+  }
+}
