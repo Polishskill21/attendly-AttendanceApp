e2e2ac41a8d0afe9a8cfd289f3e0401373f8cb18
Polishskill21 <rafalmiszkiewicz6@gmail.com>
2026-01-25 14:11:29 +0100

    feat: using enum for insertion

--- lib/data/local/tables/dialy_entry_table.dart
+++ lib/data/local/tables/dialy_entry_table.dart
@@ -1,11 +1,13 @@
 import 'package:attendly/data/local/tables/directory_people_table.dart';
+import 'package:attendly/data/local/tables/enums/category.dart';
 import 'package:drift/drift.dart';
 
 class DailyEntry extends Table{
   IntColumn get recordID => integer().named('record_id')();
   DateTimeColumn get dates => dateTime()();
   IntColumn get id => integer().references(DirectoryPeople, #id)();
-  TextColumn get category => text().check(category.isIn(['open', 'offer', 'parent', 'other']))();
+  TextColumn get categroy => textEnum<Category>()();
+  //TextColumn get category => text().check(category.isIn(['open', 'offer', 'parent', 'other']))();
   TextColumn get description => text()();
 
   @override
--- lib/data/local/tables/directory_people_table.dart
+++ lib/data/local/tables/directory_people_table.dart
@@ -1,3 +1,4 @@
+import 'package:attendly/data/local/tables/enums/gender.dart';
 import 'package:drift/drift.dart';
 
 class DirectoryPeople extends Table {
@@ -7,7 +8,9 @@ class DirectoryPeople extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get name => text().unique()();
   DateTimeColumn get birthday => dateTime()();
-  TextColumn get gender => text().check(gender.isIn(['m', 'f', 'd']))();
+  //TextColumn get gender => text().check(gender.isIn(['m', 'f', 'd']))();
+  //https://drift.simonbinder.eu/type_converters/#implicit-enum-converters:~:text=textEnum%3CStatus%3E%28%29%28%29%3B%20%7D-,Caution%20with%20enums
+  TextColumn get gender => textEnum<Gender>()();
   BoolColumn get migration => boolean()();
   TextColumn get migrationBackground => text().named('migration_background')();
 }
