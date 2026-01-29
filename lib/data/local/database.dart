d264e2ee5f92b8be67101b86043ebf71726d6066
Polishskill21 <150601593+Polishskill21@users.noreply.github.com>
2026-01-29 19:48:37 +0100

    Feature: Reading DAO (#45)
    * feat: new migration verison to account for date reading
    
    * feat: auto gen
    
    * feat: reading dao same as in the old backend
    
    * feat: auto gen
    
    * feat: date converion to save only date and time (00:00:00.000) to db and read only as datetime obj
    
    * fix: accounted for the correct column types
    
    * fix: accounted for hte correct column type
    
    * feat: moved the db init with drift to the settings page as a button to test
    
    * feat: createad a unit test for the database to be consistent and not encounter unexpected changes

--- lib/data/local/database.dart
+++ lib/data/local/database.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+import 'package:attendly/data/local/dao/read_dao.dart';
+import 'package:attendly/data/local/tables/date_only_converter.dart';
 import 'package:attendly/data/local/tables/dialy_entry_table.dart';
 import 'package:attendly/data/local/tables/directory_people_table.dart';
 import 'package:attendly/data/local/tables/enums/category.dart';
@@ -9,7 +11,10 @@ import 'package:drift/native.dart';
 
 part 'database.g.dart';
 
-@DriftDatabase(tables: [DirectoryPeople, DailyEntry, WeeklyEntry])
+@DriftDatabase(
+  tables: [DirectoryPeople, DailyEntry, WeeklyEntry],
+  daos: [ReadDao]
+)
 class AppDatabase extends _$AppDatabase{
   AppDatabase(super.executor);
 
@@ -20,7 +25,27 @@ class AppDatabase extends _$AppDatabase{
   );
 
   @override
-  int get schemaVersion => 1;
+  int get schemaVersion => 2;
+
+  @override
+  MigrationStrategy get migration {
+    return MigrationStrategy(
+      onUpgrade: (m, from, to) async {
+        await transaction(() async {
+          if (from < 2) {
+            // Appends 'T00:00:00.000' to any string that doesn't have a 'T' yet 
+            await customStatement("UPDATE all_people SET birthday = birthday || 'T00:00:00.000' WHERE birthday NOT LIKE '%T%'");
+            await customStatement("UPDATE daily_entry SET dates = dates || 'T00:00:00.000' WHERE dates NOT LIKE '%T%'");
+            await customStatement("UPDATE weekly_entry SET dates = dates || 'T00:00:00.000' WHERE dates NOT LIKE '%T%'");
+          }
+        });
+      },
+      beforeOpen: (details) async {
+        // This runs every time the database is opened 
+        await customStatement('PRAGMA foreign_keys = ON');
+      },
+    );
+  }
 
   Future<void> forceOpen() async {
     await customSelect('SELECT 1').getSingle();
--- lib/data/local/tables/dialy_entry_table.dart
+++ lib/data/local/tables/dialy_entry_table.dart
@@ -1,14 +1,14 @@
+import 'package:attendly/data/local/tables/date_only_converter.dart';
 import 'package:attendly/data/local/tables/directory_people_table.dart';
 import 'package:attendly/data/local/tables/enums/category.dart';
 import 'package:drift/drift.dart';
 
 class DailyEntry extends Table{
   IntColumn get recordID => integer().named('record_id')();
-  DateTimeColumn get dates => dateTime()();
+  TextColumn get dates => text().map(const DateOnlyConverter())();
   IntColumn get id => integer().references(DirectoryPeople, #id)();
-  TextColumn get categroy => textEnum<Category>()();
-  //TextColumn get category => text().check(category.isIn(['open', 'offer', 'parent', 'other']))();
-  TextColumn get description => text()();
+  TextColumn get category => textEnum<Category>()();
+  TextColumn get description => text().nullable()();
 
   @override
   Set<Column> get primaryKey => {recordID, dates, id};
