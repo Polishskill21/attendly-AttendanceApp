70362a779e6ca246db5c07df585389d499d1c070
Polishskill21 <150601593+Polishskill21@users.noreply.github.com>
2026-02-15 13:39:09 +0100

    Feature: Directory Page Repository (#51)
    * feat: created a repository for the directory page only for better maintance and code
    
    * fix: changed the argument to be required in the dao but optional in the repo

--- test/read_dao_test.dart
+++ test/read_dao_test.dart
@@ -28,7 +28,7 @@ void main() {
         migrationBackground: const Value('Polnisch'),
       ));
 
-      final all = await db.readDao.getAllPeople();
+      final all = await db.readDao.getAllPerson(true);
       expect(all.length, 1);
       
       final person = await db.readDao.getPersonById(all.first.id);
