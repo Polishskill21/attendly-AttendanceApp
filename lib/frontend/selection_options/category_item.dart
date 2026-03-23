448126e7ed93d920ecef380563e405e8293ce733
Polishskill21 <rafalmiszkiewicz6@gmail.com>
2026-03-23 17:58:45 +0100

    fix: adjusted to use new db enum

--- lib/frontend/selection_options/category_item.dart
+++ lib/frontend/selection_options/category_item.dart
@@ -1,4 +1,4 @@
-import 'package:attendly/backend/enums/category.dart';
+import 'package:attendly/data/local/tables/enums/category.dart';
 import 'package:attendly/localization/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
